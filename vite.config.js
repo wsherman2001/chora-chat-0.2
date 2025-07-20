5b7b7762e6f86e39ce9614be1efc5b1a51555f00
wsherman2001 <w.sherman2001@gmail.com>
2025-07-20 14:35:32 +0200

    Add missing index.html + vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3003',
-        ws: true,
-      },
-    },
-  },
+  build: { outDir: 'dist' },
 });
